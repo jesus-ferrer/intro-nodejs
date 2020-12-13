486fb7c19f201c75816b99be968130ba21ed6dbb
Jesus Ferrer <jferrer1310@hotmail.com>
2020-12-12 18:11:34 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    hola: function () {
+        console.log('Hola mundo');
+    },
+    holaPersonalizado: function (nombre) {
+        console.log('Hola ', nombre);
+    },
+    holaCurso: function () {
+        console.log('Hola al curso');
+    }
+};
